e25e46f85a6c01ccc845e311beb2ec47a585229d
ianzonja <ianzonja@foi.hr>
2016-04-11 15:14:01 +0200

    Prvi commit

---
+++ Shape recognition/Shape recognition/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using System.Drawing.Imaging;
+using AForge.Math.Geometry;
+using AForge;
+
+
+
+
+namespace Shape_recognition
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection kamere;
+        private VideoCaptureDevice kamerica;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            kamere = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo VideoCaptureDevice in kamere)
+            {
+                comboBox1.Items.Add(VideoCaptureDevice.Name);
+            }
+
+
+            comboBox1.SelectedIndex = 0;
+            kamerica = new VideoCaptureDevice();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (kamerica.IsRunning)
+            {
+                kamerica.Stop();
+                pictureBox1.Image = null;
+                pictureBox1.Invalidate();
+            }
+            else
+            {
+                kamerica = new VideoCaptureDevice(kamere[comboBox1.SelectedIndex].MonikerString);
+                kamerica.NewFrame += new NewFrameEventHandler(kamerica_NewFrame);
+
+                kamerica.Start();
+            }
+        }
+
+        private void kamerica_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+             Bitmap slika = (Bitmap)eventArgs.Frame.Clone();
+            BitmapData bitmapData = slika.LockBits(
+              new Rectangle(0, 0, slika.Width, slika.Height),
+              ImageLockMode.ReadWrite, slika.PixelFormat);
+
+            // step 1 - turn background to black
+            ColorFiltering colorFilter = new ColorFiltering();
+
+            colorFilter.Red = new IntRange(0, 64);
+            colorFilter.Green = new IntRange(0, 64);
+            colorFilter.Blue = new IntRange(0, 64);
+            colorFilter.FillOutsideRange = false;
+
+            colorFilter.ApplyInPlace(bitmapData);
+
+            // step 2 - locating objects
+            BlobCounter blobCounter = new BlobCounter();
+
+            blobCounter.FilterBlobs = true;
+            blobCounter.MinHeight = 5;
+            blobCounter.MinWidth = 5;
+
+            blobCounter.ProcessImage(bitmapData);
+            Blob[] blobs = blobCounter.GetObjectsInformation();
+            slika.UnlockBits(bitmapData);
+
+            // step 3 - check objects' type and highlight
+            SimpleShapeChecker shapeChecker = new SimpleShapeChecker();
+
+            Graphics g = Graphics.FromImage(slika);
+            Pen yellowPen = new Pen(Color.Yellow, 2); // circles
+            Pen redPen = new Pen(Color.Red, 2);       // quadrilateral
+            Pen brownPen = new Pen(Color.Brown, 2);   // quadrilateral with known sub-type
+            Pen greenPen = new Pen(Color.Green, 2);   // known triangle
+            Pen bluePen = new Pen(Color.Blue, 2);     // triangle
+
+            for (int i = 0, n = blobs.Length; i < n; i++)
+            {
+                List<IntPoint> edgePoints = blobCounter.GetBlobsEdgePoints(blobs[i]);
+                
+                // calculate center point
+                AForge.Point center;
+                // calculate radius
+                float radius;
+
+                // is circle ?
+                if (shapeChecker.IsCircle(edgePoints, out center, out radius))
+                {
+                    g.DrawEllipse(yellowPen,
+                        (float)(center.X - radius), (float)(center.Y - radius),
+                        (float)(radius * 2), (float)(radius * 2));
+                }
+                else
+                {
+                    List<IntPoint> corners;
+
+                    // is triangle or quadrilateral
+                    if (shapeChecker.IsConvexPolygon(edgePoints, out corners))
+                    {
+                        // get sub-type
+                        PolygonSubType subType = shapeChecker.CheckPolygonSubType(corners);
+
+                        Pen pen;
+
+                        if (subType == PolygonSubType.Unknown)
+                        {
+                            pen = (corners.Count == 4) ? redPen : bluePen;
+                        }
+                        else
+                        {
+                            pen = (corners.Count == 4) ? brownPen : greenPen;
+                        }
+
+                        g.DrawPolygon(pen, ToPointsArray(corners));
+                    }
+                }
+            }
+
+            yellowPen.Dispose();
+            redPen.Dispose();
+            greenPen.Dispose();
+            bluePen.Dispose();
+            brownPen.Dispose();
+            g.Dispose();
+
+
+
+            pictureBox1.Image = slika;
+
+
+
+
+        }
+
+        private System.Drawing.Point[] ToPointsArray(List<IntPoint> points)
+        {
+            System.Drawing.Point[] array = new System.Drawing.Point[points.Count];
+
+            for (int i = 0, n = points.Count; i < n; i++)
+            {
+                array[i] = new System.Drawing.Point(points[i].X, points[i].Y);
+            }
+
+            return array;
+        }
+
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (kamerica.IsRunning == true)
+            {
+                kamerica.Stop();
+            }
+        
+        }
+
+
+    }
+}
